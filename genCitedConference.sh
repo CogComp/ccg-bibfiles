1db9c8e0f8f116e3971249e054cc6c88c4b2959c
hpeng7 <hpeng7@illinois.edu>
2018-05-23 16:34:15 -0700

    update all cited

--- genCitedConference.sh
+++ genCitedConference.sh
@@ -5,8 +5,14 @@ nice perl shorten.pl cited.bib > cited-compact.bib.tmp
 cat compact_head.bib cited-compact.bib.tmp > cited-compact.bib
 rm cited-compact.bib.tmp
 
-cat full_head.bib ccg.bib > ccg-long.bib
-rm ccg-compact.bib
-nice perl shorten.pl ccg.bib > ccg-compact.bib.tmp
-cat compact_head.bib ccg-compact.bib.tmp > ccg-compact.bib
-rm ccg-compact.bib.tmp
+cat full_head.bib cited-recent.bib > cited-recent-long.bib
+#rm cited-recent-compact.bib
+nice perl shorten.pl cited-recent.bib > cited-recent-compact.bib.tmp
+cat compact_head.bib cited-recent-compact.bib.tmp > cited-recent-compact.bib
+rm cited-recent-compact.bib.tmp
+
+# cat full_head.bib ccg.bib > ccg-long.bib
+# rm ccg-compact.bib
+# nice perl shorten.pl ccg.bib > ccg-compact.bib.tmp
+# cat compact_head.bib ccg-compact.bib.tmp > ccg-compact.bib
+# rm ccg-compact.bib.tmp
